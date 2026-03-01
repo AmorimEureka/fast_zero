c43b78fc969e99e31df378db72ad10591f7237ea
rafael.amorim <raffaekk@gmail.com>
2026-03-01 17:32:31 -0300

    feat: Codigos da Aula2 do curso fastAPI - Dunossauro

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,30 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fast_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá, mundo!'}
+
+
+@app.get(
+    '/exercio_2_retorna_html',
+    status_code=HTTPStatus.OK,
+    response_class=HTMLResponse,
+)
+def exercicio_2_ola_mundo():
+    return """
+    <html>
+        <head>
+            <title> Nosso Ola Mundo!</Title>
+        </head>
+        <body>
+            <h1> Olá, Mundo!</h1>
+        </body>
+    </html>"""
--- tests/test_app.py
+++ tests/test_app.py
@@ -23,3 +23,15 @@ def test_root_deve_retornar_ola_mundo():
     # Assert
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá, mundo!'}
+
+
+def test_exercicio_2():
+    client = TestClient(app)
+
+    response = client.get('/exercio_2_retorna_html')
+
+    string_ola_mundo = response.text
+    string_padrao = '<h1> Olá, Mundo!</h1>'
+
+    assert response.status_code == HTTPStatus.OK
+    assert string_padrao in string_ola_mundo
