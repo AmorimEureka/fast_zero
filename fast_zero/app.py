9876c81b8dfda15533e872ab892d7dcfff4d2221
rafael.amorim <raffaekk@gmail.com>
2026-03-08 20:53:20 -0300

    feat: Aula 03 - FastAPI Dunossauro 	-  CRUD 	- Tests 	- Schema

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,11 +1,13 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import HTMLResponse
 
-from fast_zero.schemas import Message
+from fast_zero.schemas import Message, UserDB, UserList, UserPublic, UserSchema
 
-app = FastAPI()
+app = FastAPI(title='Projeto Amoras')
+
+database = []
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
@@ -28,3 +30,41 @@ def exercicio_2_ola_mundo():
             <h1> Olá, Mundo!</h1>
         </body>
     </html>"""
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    # breakpoint()
+    user_with_id = UserDB(
+        **user.model_dump(),
+        # username=user.username,
+        # email=user.email,
+        # password=user.password,
+        id=len(database) + 1,
+    )
+
+    database.append(user_with_id)
+
+    return user_with_id
+
+
+@app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
+def read_users():
+    return {'users': database}
+
+
+@app.put(
+    '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
+)
+def update_user(user_id: int, user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=user_id)
+
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail='Deu ruim!! Não Achem emmm!!!',
+        )
+
+    database[user_id - 1] = user_with_id
+
+    return user_with_id
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,11 +1,7 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
-
-
-def test_root_deve_retornar_ola_mundo():
+def test_root_deve_retornar_ola_mundo(client_arranj):
     """
     Teste de 3 etapas [AAA]
     - A: Arrange  -> Arranjo/Organização
@@ -15,23 +11,86 @@ def test_root_deve_retornar_ola_mundo():
     """
 
     # Arrange
-    client = TestClient(app)
+    # client = TestClient(app)
 
     # Act
-    response = client.get('/')
+    response = client_arranj.get('/')
 
     # Assert
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá, mundo!'}
 
 
-def test_exercicio_2():
-    client = TestClient(app)
+def test_exercicio_2(client_arranj):
+    # client = TestClient(app)
 
-    response = client.get('/exercio_2_retorna_html')
+    response = client_arranj.get('/exercio_2_retorna_html')
 
     string_ola_mundo = response.text
     string_padrao = '<h1> Olá, Mundo!</h1>'
 
     assert response.status_code == HTTPStatus.OK
     assert string_padrao in string_ola_mundo
+
+
+def test_create_user(client_arranj):
+    # client = TestClient(app)
+
+    response = client_arranj.post(
+        '/users/',
+        json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'id': 1,
+        'username': 'alice',
+        'email': 'alice@example.com',
+    }
+
+
+def test_read_users(client_arranj):
+    response = client_arranj.get('/users/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {'id': 1, 'email': 'alice@example.com', 'username': 'alice'},
+        ]
+    }
+
+
+def test_update_user_not_found(client_arranj):
+    response = client_arranj.put(
+        '/users/9999',
+        json={
+            'username': 'ghost',
+            'email': 'ghost@example.com',
+            'password': 'secret',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'Deu ruim!! Não Achem emmm!!!'}
+
+
+def test_update_user(client_arranj):
+    response = client_arranj.put(
+        '/users/1',
+        json={
+            'username': 'bob',
+            'email': 'bob_buxo@example.com',
+            'password': 'segrect',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'bob',
+        'email': 'bob_buxo@example.com',
+        'id': 1,
+    }
