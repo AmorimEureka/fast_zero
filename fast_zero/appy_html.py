a4bdc431f59d138e8cd5cbeb1ce9e44656e71c44
rafael.amorim <raffaekk@gmail.com>
2026-05-15 10:37:25 -0300

    feat: Exercicio - Retornando HTML em requisicao

---
+++ fast_zero/appy_html.py
@@ -0,0 +1,19 @@
+from http import HTTPStatus
+
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+
+@app.get('/', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def read_root():
+    return """
+    <html>
+        <head>
+            <title> Nosso Ola Mundo!</Title>
+        </head>
+        <body>
+            <h1> Olá, Mundo!</h1>
+        </body>
+    </html>"""
