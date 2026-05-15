9798d63d14ec1cd774ea87b489639aae89e6d302
rafael.amorim <raffaekk@gmail.com>
2026-05-15 10:49:00 -0300

    feat: Implementations: - settings: Abstratacao pydantic-settings p/ lhe dar com variaveis de ambiente
    - database: Criando sessao do banco de prod
    
    - alembic: Configuracao de migrations automaticas e primeiro versionamento
    
    - conftest: Configuracao de fixtures para os tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,23 +1,77 @@
+from contextlib import contextmanager
+from datetime import datetime
+
 import pytest
 from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
 from sqlalchemy.orm import Session
+from sqlalchemy.pool import StaticPool
 
 from fast_zero.app import app
-from fast_zero.model import table_registry
+from fast_zero.database import get_session
+from fast_zero.models import User, table_registry
 
 
 @pytest.fixture
-def client_arranj():
-    return TestClient(app)
+def client_arranj(session):
+    def get_session_override():
+        return session
+
+    # return TestClient(app)
+
+    with TestClient(app) as client:
+        app.dependency_overrides[get_session] = get_session_override
+        yield client
+
+    app.dependency_overrides.clear()
 
 
 @pytest.fixture
 def session():
-    engine = create_engine('sqlite:///:memory:')
+    engine = create_engine(
+        'sqlite:///:memory:',
+        connect_args={'check_same_thread': False},
+        poolclass=StaticPool,
+    )
     table_registry.metadata.create_all(engine)
 
     with Session(engine) as s:
         yield s
 
-    table_registry.metadata.drop_all(engine) # Teardown
+    table_registry.metadata.drop_all(engine)  # Teardown
+
+
+@contextmanager
+def _mock_db_time(model, time=datetime(2025, 5, 20)):
+
+    def fake_time_hook(mapper, connection, target):
+
+        if hasattr(target, 'created_at'):
+            target.created_at = time
+
+        # print(target)
+        # breakpoint()
+
+    event.listen(model, 'before_insert', fake_time_hook)
+
+    yield time
+
+    event.remove(model, 'before_insert', fake_time_hook)
+
+
+@pytest.fixture
+def mock_db_time():
+    return _mock_db_time
+
+
+@pytest.fixture
+def create_user(session: Session):
+
+    user = User(
+        username='alice', email='alice@example.com', password='testtest'
+    )
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+
+    return user
