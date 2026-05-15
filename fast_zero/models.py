8f1b9351db7e69783d95434227aa8ac35563930c
rafael.amorim <raffaekk@gmail.com>
2026-05-15 10:42:08 -0300

    feat: Implementando model User e Testando Insert de usuario no .db teste

--- fast_zero/models.py
+++ fast_zero/models.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm import Mapped, mapped_column, registry
 table_registry = registry()
 
 
+# @mapped_as_dataclass(table_registry)
 @table_registry.mapped_as_dataclass
 class User:
     __tablename__ = 'users'
@@ -13,7 +14,7 @@ class User:
     id: Mapped[int] = mapped_column(init=False, primary_key=True)
     username: Mapped[str] = mapped_column(unique=True)
     email: Mapped[str] = mapped_column(unique=True)
-    password: Mapped[str] = mapped_column()
+    password: Mapped[str]
     created_at: Mapped[datetime] = mapped_column(
         init=False, server_default=func.now()
     )
--- tests/test_db.py
+++ tests/test_db.py
@@ -1,12 +1,29 @@
-from fast_zero.model import User
+from dataclasses import asdict
+
+from sqlalchemy import select
+
+from fast_zero.models import User
 
 
-def test_create_user(session):
-    user = User(username='test', email='test@test.com', password='secret')
+def test_create_user(session, mock_db_time):
 
-    session.add(user)
-    session.commit()
+    with mock_db_time(model=User) as time:
+        new_user = User(
+            username='test', email='test@test.com', password='secret'
+        )
+
+        session.add(new_user)
+        session.commit()
+
+        user = session.scalar(select(User).where(User.username == 'test'))
 
     # breakpoint()
 
-    assert user.username == 'test'
+    # assert user.username == 'test'
+    assert asdict(user) == {
+        'id': 1,
+        'username': 'test',
+        'email': 'test@test.com',
+        'password': 'secret',
+        'created_at': time,
+    }
